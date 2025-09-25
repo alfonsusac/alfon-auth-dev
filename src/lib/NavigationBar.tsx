e3eddd01e582e563df3af4b91b6a729f1934df20
Alfonsus Ardani <alfonsusac@gmail.com>
2025-09-25 20:56:48 +0700

    header

--- src/lib/NavigationBar.tsx
+++ src/lib/NavigationBar.tsx
@@ -10,7 +10,7 @@ export function NavigationBar(props: {
   return (
     <>
       <div className="sticky top-0 h-(--header-h) z-(--z-navbar-sticky) contain-layout">
-        <div className="absolute w-[99999px] h-screen bg-background this-is-header-color -translate-x-[100vw] -translate-y-full top-full" />
+        <div className="absolute w-[99999px] h-screen backdrop-blur-xs bg-background/95 this-is-header-color -translate-x-[100vw] -translate-y-full top-full" />
         <div className="flex h-full items-center absolute inset-0">
           <div>
             <BackButton href={props.back?.href}>
@@ -20,7 +20,7 @@ export function NavigationBar(props: {
         </div>
       </div>
       <div className="fixed top-0 w-full left-0 z-10 z-(--z-navbar-fixed) pointer-events-none">
-        <div className="w-full h-[calc(var(--header-h)_*_1.5)] bg-gradient-to-b from-blue-950/10 via-blue-900/5" />
+        <div className="w-full h-[calc(var(--header-h)_*_1.5)] bg-gradient-to-b from-blue-950/10 via-blue-900/10 reveal-on-scroll" />
       </div>
     </>
   )
