6a38c5c99c70580bca58a4c3df34c558bd41647a
Alfonsus Ardani <alfonsusac@gmail.com>
2025-09-26 16:07:02 +0700

    refactor: remove unused router imports and clean up navigation logic

--- src/lib/link/Link.client.tsx
+++ src/lib/link/Link.client.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import NextLink from "next/link"
-import { useRouter, useSearchParams } from "next/navigation"
 import type { ComponentProps } from "react"
 
 
@@ -32,21 +31,10 @@ export function LinkClient({
       if (client) {
         e.preventDefault()
         const href = props.href?.toString() ?? "#"
-        console.log("shallow navigation to", href)
         if (replace)
           window.history.pushState({}, "", href)
         else 
           window.history.replaceState({}, "", href)
-
-        // if (replace)
-          // router.replace(href, { scroll })
-        // else
-          // router.push(href, { scroll })
-        // const url = new URL(href, location.origin)
-        // url.searchParams.forEach((value, key) => {
-        //   if (sp.get(key) === value) url.searchParams.delete(key)
-        // })
-        // router.replace(url.toString())
       }
     }}
   />
