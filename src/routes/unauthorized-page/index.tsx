a3c1901b1d849dbde46e2a084e5c49e3d430f110
Alfonsus Ardani <alfonsusac@gmail.com>
2025-10-14 23:36:28 +0700

    unauthorized page

--- src/routes/unauthorized-page/index.tsx
+++ src/routes/unauthorized-page/index.tsx
@@ -1,17 +1,21 @@
 import { secureRedirectString } from "@/lib/auth/redirect"
+import { Link } from "@/lib/link/link"
 import { Page, page } from "@/lib/page"
 import { getSingleQuery } from "@/lib/page-search-params"
 import { Header, Section, Title } from "@/lib/primitives"
 import { Logo } from "@/shared/logot"
+import { route } from "../routes"
 
 export default page('/unauthorized', async page => {
-
-  const next_url = secureRedirectString(decodeURIComponent(getSingleQuery(page.searchParams.next)))
-
   return <>
     <Section>
-      <Title>You are unauthorized to do that!</Title>
-      <div></div>
+      <Title>Sorry, you are unauthorized to do that :(</Title>
+      <div>
+        You do not have the necessary permissions to access this page or perform this action.
+      </div>
+    </Section>
+    <Section>
+      <Link className="button primary" href={route.home}>Back to Home</Link>
     </Section>
   </>
 }, children => <>
